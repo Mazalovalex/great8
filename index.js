6b8324f21ded01ac4957160b3b2471a368785e02
Mazalov Alex <mazalovalexey1995@yandex.ru>
2025-02-05 19:47:07 +0300

    v1.24

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function startImageAnimation(imagesArr) {
 
   let currentIndex = 0;
   const imageElement = document.querySelector(".ovechkin-image");
-  const frameDelay = 2500;
+  const frameDelay = 100;
 
   const interval = setInterval(function () {
     if (currentIndex >= imagesArr.length) {
