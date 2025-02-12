b92bd50ee0b656a1f513ad3825b3c98b3d066513
Mazalov Alex <mazalovalexey1995@yandex.ru>
2025-02-12 12:18:21 +0300

    v1.62

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,14 +2,37 @@ const popupPlayers = document.querySelector(".popup");
 const topScorerItems = document.querySelectorAll(".top-scorer-item");
 const popupButtonElements = document.querySelectorAll(".popup__button-close");
 console.log(popupButtonElements);
-
 // Открытие модального окна
+
+
 function openModal(modalElement) {
   modalElement.classList.add("popup_is-opened");
   modalElement.scrollTop = 0; // Сброс позиции скролла на верх
   modalElement.addEventListener("mousedown", handleCloseOverlay);
   document.addEventListener("keydown", handleCloseEsc);
 }
+
+
+function openModal(modalElement) {
+  modalElement.classList.add("popup_is-opened");
+
+  setTimeout(() => {
+    const popupContent = modalElement.querySelector(".popup__content");
+    if (popupContent) {
+      console.log("До сброса scrollTop:", popupContent.scrollTop);
+      popupContent.scrollTop = 0;
+      console.log("После сброса scrollTop:", popupContent.scrollTop);
+    } else {
+      console.log("popup__content не найден!");
+    }
+  }, 50); // Даем время на рендеринг
+
+  modalElement.addEventListener("mousedown", handleCloseOverlay);
+  document.addEventListener("keydown", handleCloseEsc);
+}
+
+
+
 
 // Закрытие модального окна
 function closeModal(modalElement) {
@@ -29,7 +52,7 @@ function handleCloseOverlay(evt) {
 function handleCloseEsc(evt) {
   if (evt.key === "Escape") {
     const openPopup = document.querySelector(".popup_is-opened");
-    closeModal(openPopup);
+    if (openPopup) closeModal(openPopup);
   }
 }
 
@@ -40,10 +63,15 @@ topScorerItems.forEach((topScorerItem) => {
   });
 });
 
-// Обработчик для кнопок закрытия попапа
-popupButtonElements.forEach(function (popupButtonClose) {
+
+
+
+
+
+// Обработчик для всех кнопок закрытия попапа
+popupButtonElements.forEach((popupButtonClose) => {
   popupButtonClose.addEventListener("click", function (evt) {
     const modal = evt.target.closest(".popup");
-    closeModal(modal);
+    if (modal) closeModal(modal);
   });
 });
